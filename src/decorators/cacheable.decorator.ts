86058a979f8213ec52464c773e49016dfe68dd3c
louis amstutz <louis.amstutz@gmail.com>
2019-11-27 18:14:12 +0100

    remove level on cache decorators

--- src/decorators/cacheable.decorator.ts
+++ src/decorators/cacheable.decorator.ts
@@ -10,49 +10,46 @@ function getCacheId(targetClass: MFDao<any>, methodName: string, params: any[]):
   return `dao(${targetClass.mustachePath}).${methodName}(${Flatted.stringify({ params })})`;
 }
 
-export function NoCache() {
-  return (target: Object) => {
-    Reflect.defineMetadata('cacheable', false, target);
-  };
+export function NoCache(target: Object) {
+  Reflect.defineMetadata('cacheable', false, target);
 }
 
 
-export function Cacheable() {
-  return (
-    targetClass: MFDao<any>,
-    methodName: string,
-    propertyDesciptor: PropertyDescriptor): PropertyDescriptor => {
-    if (targetClass.isCacheable()) {
-      const originalMethod: (...args: any[]) => Observable<any> = propertyDesciptor.value;
+export function Cacheable(
+  targetClass: MFDao<any>,
+  methodName: string,
+  propertyDesciptor: PropertyDescriptor): PropertyDescriptor {
 
-      propertyDesciptor.value = function (...args: any[]) {
+  if (targetClass.isCacheable()) {
+    const originalMethod: (...args: any[]) => Observable<any> = propertyDesciptor.value;
 
-        const lastArgument = args.length ? args[args.length - 1] : null;
+    propertyDesciptor.value = function (...args: any[]) {
 
-        const cachableIsDisabled = lastArgument &&
-          typeof lastArgument === 'object' &&
+      const lastArgument = args.length ? args[args.length - 1] : null;
+
+      const cachableIsDisabled = lastArgument &&
+        typeof lastArgument === 'object' &&
+        (
           (
-            (
-              typeof (lastArgument as any).cacheable === 'boolean' &&
-              (lastArgument as any).cacheable === false
-            ) ||
-            (lastArgument as any).completeOnFirst === true
-          );
+            typeof (lastArgument as any).cacheable === 'boolean' &&
+            (lastArgument as any).cacheable === false
+          ) ||
+          (lastArgument as any).completeOnFirst === true
+        );
 
-        if (!cachableIsDisabled) {
-          const cacheId = getCacheId(targetClass, methodName, args);
-          if (!MFCache.cache[cacheId]) {
-            const subject = new ReplaySubject(1);
-            MFCache.cache[cacheId] = {
-              subject,
-              subscription: originalMethod.apply(this, args).subscribe(doc => subject.next(doc))
-            };
-          }
-          return MFCache.cache[cacheId].subject;
+      if (!cachableIsDisabled) {
+        const cacheId = getCacheId(targetClass, methodName, args);
+        if (!MFCache.cache[cacheId]) {
+          const subject = new ReplaySubject(1);
+          MFCache.cache[cacheId] = {
+            subject,
+            subscription: originalMethod.apply(this, args).subscribe(doc => subject.next(doc))
+          };
         }
-        return originalMethod.apply(this, args);
-      };
-      return propertyDesciptor;
-    }
-  };
+        return MFCache.cache[cacheId].subject;
+      }
+      return originalMethod.apply(this, args);
+    };
+    return propertyDesciptor;
+  }
 }
