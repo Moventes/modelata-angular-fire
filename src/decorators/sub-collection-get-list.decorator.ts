4a3f1677c5142fb0bd66a94b34205ca850a76ce5
louis amstutz <louis.amstutz@moventes.com>
2019-11-29 17:35:33 +0100

    optional options

--- src/decorators/sub-collection-get-list.decorator.ts
+++ src/decorators/sub-collection-get-list.decorator.ts
@@ -1,7 +1,7 @@
 import { IMFGetListOptions } from '@modelata/types-fire/lib/angular';
 import 'reflect-metadata';
 
-export function SubCollectionGetList<M = any>(collectionName: string, daoName: string, options: IMFGetListOptions<M>): any {
+export function SubCollectionGetList<M = any>(collectionName: string, daoName: string, options?: IMFGetListOptions<M>): any {
     return (target: any, propertyKey: string) => {
         Reflect.defineMetadata(
             'observableFromSubCollection',
